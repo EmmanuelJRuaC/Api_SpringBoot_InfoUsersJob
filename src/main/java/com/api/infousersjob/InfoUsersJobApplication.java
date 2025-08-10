dd58a3d20382656629d0d4179e08b4f5039ba7c7
Emmanuel <eruacaballero@gmail.com>
2025-08-09 23:45:56 -0500

    Metodo redireccionamiento a UsersController

--- src/main/java/com/api/infousersjob/InfoUsersJobApplication.java
+++ src/main/java/com/api/infousersjob/InfoUsersJobApplication.java
@@ -1,7 +1,12 @@
 package com.api.infousersjob;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 public class InfoUsersJobApplication {
@@ -10,4 +15,9 @@ public class InfoUsersJobApplication {
 		SpringApplication.run(InfoUsersJobApplication.class, args);
 	}
 
+	// Redireccionamiento al controlador "UsersControllers"
+	@RequestMapping("/")
+	public void redirigir(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/users/getusers");
+	}
 }
