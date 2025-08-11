850437f005bf84206f32468ae37b513a6a495d70
Emmanuel <eruacaballero@gmail.com>
2025-08-10 22:31:29 -0500

    Nuevo metodo get

--- src/main/java/com/api/infousersjob/controllers/UsersController.java
+++ src/main/java/com/api/infousersjob/controllers/UsersController.java
@@ -1,5 +1,37 @@
 package com.api.infousersjob.controllers;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.infousersjob.models.UsersModel;
+import com.api.infousersjob.query_dao.dao.UsersDAO;
+
+@RestController
+@RequestMapping("/users")
 public class UsersController {
     
+    @Autowired
+    private UsersDAO usersDAO;
+
+    // Metodo para obtener toda la lista de usuarios
+    @GetMapping("/getusers")
+    public ResponseEntity<?> getUsers() throws SQLException {
+        ArrayList<UsersModel> users = usersDAO.getUsers();
+
+        if (users.isEmpty()) {
+        return ResponseEntity.
+            status(HttpStatus.NOT_FOUND).
+            body(Map.of("Mensaje", "No se encontraron usuarios"));
+        }
+
+        return ResponseEntity.ok(users);
+    }
 }
