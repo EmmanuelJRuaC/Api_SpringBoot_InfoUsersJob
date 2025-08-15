92470212d38e1118b9f2d80ce6f1e4de02cf63e3
Emmanuel <eruacaballero@gmail.com>
2025-08-15 16:16:28 -0500

    nuevo metodo

--- src/main/java/com/api/infousersjob/query_jpa/repository/IUsersRepository.java
+++ src/main/java/com/api/infousersjob/query_jpa/repository/IUsersRepository.java
@@ -1,5 +1,15 @@
 package com.api.infousersjob.query_jpa.repository;
 
-public interface IUsersRepository {
+import java.util.ArrayList;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.api.infousersjob.models.UsersModel;
+
+public interface IUsersRepository extends JpaRepository<UsersModel, Long> {
+
+    // Retorna todo los usuarios
+    @Query("SELECT u FROM UsersModel u INNER JOIN FETCH u.salary")
+    ArrayList<UsersModel> getUsers();
 }
